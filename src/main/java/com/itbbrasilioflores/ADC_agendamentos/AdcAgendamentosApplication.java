23afd082bc1dc458164b72a4ddc4afdb005286f8
RodrigodSBJunior <rodrigosbjunior22@gmail.com>
2025-10-06 21:54:45 -0300

    confirmo

---
+++ src/main/java/com/itbbrasilioflores/ADC_agendamentos/AdcAgendamentosApplication.java
@@ -0,0 +1,13 @@
+package com.itbbrasilioflores.ADC_agendamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdcAgendamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdcAgendamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itbbrasilioflores/ADC_agendamentos/AdcAgendamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itbbrasilioflores.ADC_agendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdcAgendamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
